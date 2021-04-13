910f1566e085bc2267f319ca092bee343af477ab
pengjiaxin1 <pengjiaxin1@xiaomi.com>
2021-04-13 17:28:44 +0800

    day1

--- day1-http/base1/main.go
+++ day1-http/base1/main.go
@@ -6,26 +6,23 @@ import (
 	"net/http"
 )
 
-/**
-net/http库的简单使用
-*/
-
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/hello", helloHandler)
 	/**
-	- 第一个参数代表地址
-	- 第二个参数代表处理所有的HTTP请求的实例（因为上面已经写了两个处理器，所以这里第二个参数为nil）
-	*/
+		用来启动Web服务
+			- addr: 代表监听的端口
+			- handler: 代表处理所有的HTTP请求的实例, nil表示用标准库中的实例处理
+	 */
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	_, _ = fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 }
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		_, _ = fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
 }
